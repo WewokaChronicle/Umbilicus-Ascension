a16657b4d17c07cc2b8a376de07b0d8e89fbf6e0
Ian Coleman <colemip@gmail.com>
2015-04-04 20:15:37 -0400

    [#25] Fixing issue with gaps between instances of background decoration

--- Assets/Scripts/Environment/BGDecoration.cs
+++ Assets/Scripts/Environment/BGDecoration.cs
@@ -1,17 +1,20 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class BGDecoration:MonoBehaviour {
+public class BGDecoration:MonoBehaviour
+{
 	private bool spawnedNextSegment = false;
 
-	public void Awake() {
+	public void Awake()
+	{
 		GetComponent<Rigidbody2D>().velocity = Vector3.up * Block.SPEED * 0.8f;
 	}
 
-	public void Update() {
+	public void Update()
+	{
 		if(!spawnedNextSegment && transform.position.y >= 0f) {
 			spawnedNextSegment = true;
-			GameObject wall = (GameObject) Instantiate(gameObject, transform.position + Vector3.down * 101.4f, Quaternion.identity);
+			GameObject wall = (GameObject)Instantiate(gameObject, transform.position + Vector3.down * 20f, Quaternion.identity);
 			// This happens after awake so our if "RightWall" still works
 			wall.name = this.name;
 		}
