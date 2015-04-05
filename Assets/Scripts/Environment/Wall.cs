2d2c9748bd76e87b7aea9928f1663410dfccd5cf
Ian Coleman <colemip@gmail.com>
2015-04-04 21:19:13 -0400

    [#26] Fixing issue with side spikes have x-pos far away from the wall they were meant to be attached to

--- Assets/Scripts/Environment/Wall.cs
+++ Assets/Scripts/Environment/Wall.cs
@@ -1,28 +1,31 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Wall:MonoBehaviour {
+public class Wall:MonoBehaviour
+{
 	public GameObject sideSpikePrefab;
 
 	private bool spawnedNextSegment = false;
 
-	public void Awake() {
+	public void Awake()
+	{
 		GetComponent<Rigidbody2D>().velocity = Vector3.up * Block.SPEED;
 		// Only do this on the first wall segment, not any clone
-		if(name == "RightWall" || name == "LeftWall") {
-			transform.Translate(Vector3.up * Random.Range(0f, 40f));
-		}
+//		if(name == "RightWall" || name == "LeftWall") {
+//			transform.Translate(Vector3.up * Random.Range(0f, 40f));
+//		}
 	}
 
-	public void Update() {
-		if(!spawnedNextSegment && transform.position.y > 50f) {
+	public void Update()
+	{
+		if(!spawnedNextSegment && transform.position.y > -9f) {
 			spawnedNextSegment = true;
-			GameObject wall = (GameObject) Instantiate(gameObject, transform.position + Vector3.down * 108f, transform.rotation);
+			GameObject wall = (GameObject)Instantiate(gameObject, transform.position + Vector3.down * 20f, transform.rotation);
 			// This happens after awake so our if "RightWall" still works
 			wall.name = this.name;
 		}
 		// Kill us once we're off the screen
-		if(transform.position.y > 120f) {
+		if(transform.position.y > 40f) {
 			Destroy(gameObject);
 		}
 	}
