b162e094eadcd3be729d53b81a23a2a666682ea5
Ian Coleman <colemip@gmail.com>
2015-04-11 23:48:46 -0400

    [#45] Refactoring heavily to increase cohesion between blocks and platforms

--- Assets/Scripts/Environment/Platform.cs
+++ Assets/Scripts/Environment/Platform.cs
@@ -4,9 +4,17 @@ using System.Collections;
 public class Platform : MonoBehaviour
 {
 	public const float SPEED = -1f;
+	public bool starterPlatform = false;
+	public GameObject blockPrefab;
+	private const int MAX_BLOCKS = 4;
+	private const float MAX_BLOCK_X = 6f;
+	private const float BLOCK_OFFSET_X = 1.2f;
+	private GameObject[] blocks;
 
 	void Awake()
 	{
+		this.blocks = new GameObject[MAX_BLOCKS];
+		this.Spawn();
 		GetComponent<Rigidbody2D>().velocity = Vector2.up * SPEED;
 	}
 
@@ -21,6 +29,29 @@ public class Platform : MonoBehaviour
 	public void OnCollisionEnter2D(Collision2D coll)
 	{
 		Debug.Log("platform collision");
+	}
+
+	public void Spawn()
+	{
+		Debug.Log("spawning platform");
+		//		GameObject platform = new GameObject("Platform");
+		
+		int blockCount = Random.Range(1, MAX_BLOCKS + 1);
+
+		Vector3 blockPosition = new Vector3(transform.position.x, transform.position.y);
+		for(int i=0; i < blockCount; i++) {
+			if(blockPosition.x <= MAX_BLOCK_X) {
+				this.blocks[i] = (GameObject)GameObject.Instantiate(blockPrefab, blockPosition, Quaternion.identity);
+				blockPosition.x += BLOCK_OFFSET_X;
+			}
+		}
+		
+		// Add edge collider with length based on block count
+		gameObject.AddComponent<EdgeCollider2D>();
+		Vector2 startPoint = transform.position + new Vector3(-BLOCK_OFFSET_X / 2f, 1.2f / 2f);
+		Vector2 endPoint = startPoint + new Vector2(BLOCK_OFFSET_X * blockCount, 0);
+		Vector2[] points = new Vector2[]{startPoint, endPoint};
+		gameObject.GetComponent<EdgeCollider2D>().points = points;
 	}
 }
 
--- Assets/Scripts/GameManagers/PlatformGenerator.cs
+++ Assets/Scripts/GameManagers/PlatformGenerator.cs
@@ -4,7 +4,6 @@ using System.Collections;
 
 public class PlatformGenerator : MonoBehaviour
 {
-	public GameObject blockPrefab;
 	public GameObject platformPrefab;
 	private const float SPAWN_TIME = 4f;
 	private float spawnTimer = 0f;
@@ -13,13 +12,9 @@ public class PlatformGenerator : MonoBehaviour
 	private const int MAX_BLOCKS = 4;
 	private const float MAX_BLOCK_X = 6f;
 	private const float BLOCK_OFFSET_X = 1.2f;
-//	private EdgeCollider2D edgeCollider;
-	private GameObject[] blocks;
-
 
 	public void Start()
 	{
-		this.blocks = new GameObject[MAX_BLOCKS];
 		Time.timeScale = 0f;
 		startTime = Time.realtimeSinceStartup;
 	}
@@ -41,27 +36,7 @@ public class PlatformGenerator : MonoBehaviour
 	
 	public void SpawnPlatform()
 	{
-//		GameObject platform = new GameObject("Platform");
-
-		int blockCount = Random.Range(1, MAX_BLOCKS + 1);
-		Vector3 blockPosition = new Vector3(Random.Range(-MAX_BLOCK_X, MAX_BLOCK_X), 10f);
-		for(int i=0; i < blockCount; i++) {
-			if(blockPosition.x <= MAX_BLOCK_X) {
-				this.blocks[i] = (GameObject)GameObject.Instantiate(blockPrefab, blockPosition, Quaternion.identity);
-				blockPosition.x += BLOCK_OFFSET_X;
-			}
-		}
-
-		Vector3 platformPosition = this.blocks[0].transform.position;
+		Vector3 platformPosition = new Vector3(Random.Range(-MAX_BLOCK_X, MAX_BLOCK_X), 10f);
 		GameObject platform = (GameObject)GameObject.Instantiate(platformPrefab, platformPosition, Quaternion.identity);
-
-		// Add edge collider with length based on block count
-		platform.AddComponent<EdgeCollider2D>();
-		Vector2 startPoint = transform.position + new Vector3(-BLOCK_OFFSET_X / 2f, 1.2f / 2f);
-		Vector2 endPoint = startPoint + new Vector2(BLOCK_OFFSET_X * blockCount, 0);
-		Vector2[] points = new Vector2[]{startPoint, endPoint};
-		platform.GetComponent<EdgeCollider2D>().points = points;
-		// reset blocks array
-		this.blocks = new GameObject[MAX_BLOCKS];
 	}
 }
